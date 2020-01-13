61d06049bea299e0f40d839beaa970fbee6d4407
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-13 14:46:38 +0530

    ANN Architecture

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -1,15 +1,18 @@
-import nltk 
+import nltk
 from nltk.stem.lancaster import LancasterStemmer
-import numpy as np
+stemmer = LancasterStemmer()
+
+import numpy
 import tflearn
-import tensorflow as tf
+import tensorflow
 import random
 import json
 
-nltk.download("punkt")
-stemmer = LancasterStemmer()
+
 with open("intents.json") as file:
     data = json.load(file)
+
+
 
 words = []
 labels = []
@@ -20,7 +23,7 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
@@ -53,6 +56,19 @@ for x, doc in enumerate(docs_x):
     training.append(bag)
     output.append(output_row)
 
-training = np.array(training)
-output = np.array(output)
 
+training = numpy.array(training)
+output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
